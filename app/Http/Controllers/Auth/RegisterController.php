9b01259777fa635e1ba46d88a94ce5922de8678e
Shubhendu S <shubhendu136508@gmail.com>
2017-05-24 13:26:37 +0530

    pagination9

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-//    protected $redirectTo = '/f';
+    protected $redirectTo = '';
 
     /**
      * Create a new controller instance.
